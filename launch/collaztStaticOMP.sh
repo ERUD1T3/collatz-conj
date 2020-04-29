78e1fe8b1df0926021009aea94b756face200f1c
ERUD1T3 <josiasmoukpe@gmail.com>
2020-04-28 20:00:00 -0400

    update

--- launch/collaztStaticOMP.sh
+++ launch/collaztStaticOMP.sh
@@ -6,12 +6,12 @@
 #SBATCH --output=outputs/collatzStaticOMP.%J.out
 #SBATCH --error=outputs/collatzStaticOMP.%J.err
 #
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24   ## controls number of threads
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=8   ## controls number of threads
 #SBATCH --time=5:00
 #SBATCH --partition=class
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 ## change n to modify number gridpoints in the rod
 ## change s to modify number of outer jacobi sweeps or iterations
-./bin/ccs 2000
+./bin/ccs 2000000
